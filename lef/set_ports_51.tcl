1c499383e809b02f664829eab4edefce54ad7bb5
ChiranjitPatel <chiranjitpatel08@gmail.com>
2026-05-04 22:06:28 +0530

    update drc

---
+++ lef/set_ports_51.tcl
@@ -0,0 +1,120 @@
+port VGND class INOUT
+port VDPWR class INOUT
+port clk class INPUT
+port rst_n class INPUT
+port ena class INPUT
+
+port ua\[0\] class INOUT
+port ua\[1\] class INOUT
+port ua\[2\] class INOUT
+port ua\[3\] class INOUT
+port ua\[4\] class INOUT
+port ua\[5\] class INOUT
+port ua\[6\] class INOUT
+port ua\[7\] class INOUT
+
+port uo_out\[0\] class OUTPUT
+port uo_out\[1\] class OUTPUT
+port uo_out\[2\] class OUTPUT
+port uo_out\[3\] class OUTPUT
+port uo_out\[4\] class OUTPUT
+port uo_out\[5\] class OUTPUT
+port uo_out\[6\] class OUTPUT
+port uo_out\[7\] class OUTPUT
+
+port uio_in\[0\] class INPUT
+port uio_in\[1\] class INPUT
+port uio_in\[2\] class INPUT
+port uio_in\[3\] class INPUT
+port uio_in\[4\] class INPUT
+port uio_in\[5\] class INPUT
+port uio_in\[6\] class INPUT
+port uio_in\[7\] class INPUT
+
+port uio_out\[0\] class OUTPUT
+port uio_out\[1\] class OUTPUT
+port uio_out\[2\] class OUTPUT
+port uio_out\[3\] class OUTPUT
+port uio_out\[4\] class OUTPUT
+port uio_out\[5\] class OUTPUT
+port uio_out\[6\] class OUTPUT
+port uio_out\[7\] class OUTPUT
+
+port uio_oe\[0\] class OUTPUT
+port uio_oe\[1\] class OUTPUT
+port uio_oe\[2\] class OUTPUT
+port uio_oe\[3\] class OUTPUT
+port uio_oe\[4\] class OUTPUT
+port uio_oe\[5\] class OUTPUT
+port uio_oe\[6\] class OUTPUT
+port uio_oe\[7\] class OUTPUT
+
+port ui_in\[0\] class INPUT
+port ui_in\[1\] class INPUT
+port ui_in\[2\] class INPUT
+port ui_in\[3\] class INPUT
+port ui_in\[4\] class INPUT
+port ui_in\[5\] class INPUT
+port ui_in\[6\] class INPUT
+port ui_in\[7\] class INPUT
+
+
+port VGND use GROUND
+port VDPWR use POWER
+port clk use SIGNAL
+port rst_n use SIGNAL
+port ena use SIGNAL
+
+port ua\[0\] use SIGNAL
+port ua\[1\] use SIGNAL
+port ua\[2\] use SIGNAL
+port ua\[3\] use SIGNAL
+port ua\[4\] use SIGNAL
+port ua\[5\] use SIGNAL
+port ua\[6\] use SIGNAL
+port ua\[7\] use SIGNAL
+
+port uo_out\[0\] use SIGNAL
+port uo_out\[1\] use SIGNAL
+port uo_out\[2\] use SIGNAL
+port uo_out\[3\] use SIGNAL
+port uo_out\[4\] use SIGNAL
+port uo_out\[5\] use SIGNAL
+port uo_out\[6\] use SIGNAL
+port uo_out\[7\] use SIGNAL
+
+port uio_in\[0\] use SIGNAL
+port uio_in\[1\] use SIGNAL
+port uio_in\[2\] use SIGNAL
+port uio_in\[3\] use SIGNAL
+port uio_in\[4\] use SIGNAL
+port uio_in\[5\] use SIGNAL
+port uio_in\[6\] use SIGNAL
+port uio_in\[7\] use SIGNAL
+
+port uio_out\[0\] use SIGNAL
+port uio_out\[1\] use SIGNAL
+port uio_out\[2\] use SIGNAL
+port uio_out\[3\] use SIGNAL
+port uio_out\[4\] use SIGNAL
+port uio_out\[5\] use SIGNAL
+port uio_out\[6\] use SIGNAL
+port uio_out\[7\] use SIGNAL
+
+port uio_oe\[0\] use SIGNAL
+port uio_oe\[1\] use SIGNAL
+port uio_oe\[2\] use SIGNAL
+port uio_oe\[3\] use SIGNAL
+port uio_oe\[4\] use SIGNAL
+port uio_oe\[5\] use SIGNAL
+port uio_oe\[6\] use SIGNAL
+port uio_oe\[7\] use SIGNAL
+
+port ui_in\[0\] use SIGNAL
+port ui_in\[1\] use SIGNAL
+port ui_in\[2\] use SIGNAL
+port ui_in\[3\] use SIGNAL
+port ui_in\[4\] use SIGNAL
+port ui_in\[5\] use SIGNAL
+port ui_in\[6\] use SIGNAL
+port ui_in\[7\] use SIGNAL
